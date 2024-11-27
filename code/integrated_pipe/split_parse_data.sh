5fe6e7098b37506aa1fa805af448dee035b5b744
Bonita van Waardenburg <bonita.vanwaardenburg@crg.eu>
2024-11-27 15:01:41 +0100

    cleaned up code and included zipped outputs

--- code/integrated_pipe/split_parse_data.sh
+++ code/integrated_pipe/split_parse_data.sh
@@ -5,21 +5,23 @@
 ####################
 res_dir=$1
 sample_id=$2
-fastq_files=$3
+fastq_files=($3) 	# converted to an array for splicing read names
 barcode_path=$4		# directory with the barcode files for each round
-output_dir=$5
 
 echo "============================"
 echo "res_dir: ${res_dir}"
 echo "sample_id: ${sample_id}"
 echo "fastq_files: ${fastq_files}"
 echo "barcode_path: ${barcode_path}"
-echo "output_dir: ${output_dir}"
+echo "output_dir: "
 echo "============================"
 
 # retrieve FASTQ file names from array
 read_1=${fastq_files[0]}
 read_2=${fastq_files[1]}
+
+echo "read 1: ${read_1}"
+echo "read 2: ${read_2}"
 
 # selects the barcode files (for chemistry v3!!!)
 bc_1="${barcode_path}/bc_data_n26_R1_v3_4.csv"
@@ -52,26 +54,26 @@ do
   echo "${name}: ${num_wells}"
   
   barcodes_r1=$( cat ${bc_1} | grep -E ",A[${num_wells}]," | awk -F ',' '{print $2}' | sed ':a;N;$!ba;s/\n/\|/g' )
-  # barcodes_r2=$( cat $bc_2 | grep -E ",A[${num_wells}]," | awk -F',' '{print $2}' | sed ':a;N;$!ba;s/\n/\|/g' )
-  # barcodes_r3=$( cat $bc_3 | grep -E ",A[${num_wells}]," | awk -F',' '{print $2}' | sed ':a;N;$!ba;s/\n/\|/g' )
-  # echo "round 1: '${barcodes_r1}' \n round 2: '${barcodes_r2}' \n round 3: '${barcodes_r3}'"
-  
+  echo "barcodes matched: ${barcodes_r1}"
+
   # Step 1: Select sequences matching the barcodes and write to file
   # version 2 allows for n number of mismatces in the barcode
   echo "${read_2}"
-  zcat "${read_2}" | grep --no-group-separator -B 1 -A 2 -E "${barcodes_r1}"| gzip  > "${output_dir}/${sample_id}_${name}_R2.fastq.gz"
-  echo "Step 1 completed, created ${output_dir}/${sample_id}_${name}_R2.fastq.gz"
+  zcat "${read_2}" | grep --no-group-separator -B 1 -A 2 -E "${barcodes_r1}" > "${sample_id}_${name}_R2.fastq"
+  echo "Step 1 completed, created ${sample_id}_${name}_R2.fastq"
 
   # Step 2: Extract headers from the filtered R2 FASTQ file
-  zcat "${output_dir}/${sample_id}_${name}_R2.fastq.gz" | grep '^@' |  sed 's/^@//g' | awk '{print $1}' > "${output_dir}/${sample_id}_${name}_headers_R2.txt"
-  echo "Step 2 completed, created ${output_dir}/filtered_headers_R2.txt"
+  cat "${sample_id}_${name}_R2.fastq" | grep '^@' |  sed 's/^@//g' | awk '{print $1}' > "${sample_id}_${name}_headers_R2.txt"
+  echo "Step 2 completed, created filtered_headers_R2.txt"
 
   # Step 3
-  seqtk subseq ${read_1} "${output_dir}/${sample_id}_${name}_headers_R2.txt" | gzip > "${output_dir}/${sample_id}_${name}_R1.fastq.gz"
-  echo "Step 3 completed, created ${output_dir}/${sample_id}_${name}_R1.fastq.gz"
+  seqtk subseq ${read_1} "${sample_id}_${name}_headers_R2.txt" > "${sample_id}_${name}_R1.fastq"
+  echo "Step 3 completed, created ${sample_id}_${name}_R1.fastq"
 
   # Step 4: Remove intermediate files
-  rm "${output_dir}/${sample_id}_${name}_headers_R2.txt"
+  rm "${sample_id}_${name}_headers_R2.txt"
+  gzip "${sample_id}_${name}_R2.fastq"
+  gzip "${sample_id}_${name}_R1.fastq"
 
 done 6< ${sw_file}
 
