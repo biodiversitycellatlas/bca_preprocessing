5d661be51d7429a166cfe0b68a1efb381f02f0e8
Bonita van Waardenburg <bonita.vanwaardenburg@crg.eu>
2024-12-19 17:12:14 +0100

    created script for inspecting unmapped reads with trinity tool, also created environment for it

---
+++ scripts/trinity.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+##################
+# slurm settings #
+##################
+#SBATCH --output=/users/asebe/bvanwaardenburg/git/bca_preprocessing/logs/%x.%j.out
+#SBATCH --error=/users/asebe/bvanwaardenburg/git/bca_preprocessing/logs/%x.%j.err
+#SBATCH --time=00:50:00
+#SBATCH --qos=vshort
+#SBATCH --mem=64G
+#SBATCH --job-name trinity
+
+module load GCC
+
+Trinity \
+  --seqType fa \
+  --max_memory 500G \
+  --single unmapped.fasta \
+  --CPU 32 \
+  --SS_lib_type F \
+  --output Trinity_Assembled_unmapped_reads \
+  --full_cleanup
+
