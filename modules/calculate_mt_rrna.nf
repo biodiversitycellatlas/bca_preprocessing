4f904d5fe2e36451dcda53cde23d56d80d8e1638
Bonita van Waardenburg <bonita.vanwaardenburg@crg.eu>
2025-01-15 09:35:54 +0100

    included calculations of mtDNA and rRNA for multimappers

--- modules/calculate_mt_rrna.nf
+++ modules/calculate_mt_rrna.nf
@@ -18,19 +18,20 @@ process CALC_MT_RRNA {
     echo "Config name: ${config_name}"
     echo "Mapping files: ${mapping_files}"
 
-    # Calculation rRNA
-    featureCounts \\
-        -t rRNA \\
-        -a ${params.ref_star_gtf} \\
-        -o feat_counts_rRNA.txt \\
-        Aligned.sortedByCoord.out.bam
+    # Create seperate BAM with filtered results - filter for multimapped reads only present in primary alignment
+    samtools view -F 256 Aligned.sortedByCoord.out.bam | grep -E "^\@|NH:i:[2-9]" > multimapped_primealign.bam
+    # Create seperate BAM with filtered results - filter for multimapped reads present in both secondary and primary alignments
+    samtools view Aligned.sortedByCoord.out.bam | grep -E "^\@|NH:i:[2-9]" > multimapped_allalign.bam
 
+    # Calculation rRNA
+    featureCounts -t rRNA -a ${params.ref_star_gtf} -o feat_counts_rRNA.txt Aligned.sortedByCoord.out.bam
+    featureCounts -t rRNA -a ${params.ref_star_gtf} -o feat_counts_rRNA_mmpa.txt multimapped_primealign.bam
+    featureCounts -t rRNA -a ${params.ref_star_gtf} -o feat_counts_rRNA_mmaa.txt multimapped_allalign.bam
+    
     # Calculation mtDNA
     grep 'mtDNA' ${params.ref_star_gtf} > mtDNA_only.gtf
-    featureCounts \\
-        -t exon \\
-        -a mtDNA_only.gtf \\
-        -o feat_counts_mtDNA.txt \\
-        Aligned.sortedByCoord.out.bam
+    featureCounts -t exon -a mtDNA_only.gtf -o feat_counts_mtDNA.txt Aligned.sortedByCoord.out.bam
+    featureCounts -t exon -a mtDNA_only.gtf -o feat_counts_mtDNA_mmpa.txt multimapped_primealign.bam
+    featureCounts -t exon -a mtDNA_only.gtf -o feat_counts_mtDNA_mmaa.txt multimapped_allalign.bam
     """
 }
--- seperate_sbatchs/calculate_mt_rrna.sh
+++ seperate_sbatchs/calculate_mt_rrna.sh
@@ -22,23 +22,35 @@ echo [$(date +"%Y-%m-%d %H:%M:%S")] starting on $(hostname)
 ###############
 
 resDir="/users/asebe/bvanwaardenburg/git/data/240810_ParseBio_Nvec_Tcas/Nvec_BCA001_BCA002_TestREF"
-output_dir="${resDir}/mapping_STARsolo_N/BCA001_lib_13077AAF_CAGATCAC-ATGTGAAG_ACMEsorb_GM"
 gtf_file="${resDir}/genome/Nvec_v5_merged_annotation_sort.gtf"
 
-featureCounts -M --fraction -f \
-	-t mtDNA \
-	-a ${gtf_file} \
-	-o ${output_dir}/feat_counts_mtDNA.txt \
-	${output_dir}/Aligned.sortedByCoord.out.bam
 
+# For each STARsolo mapping directory
+for map_dir in ${resDir}/mapping_STARsolo_*; do
+    echo "mapping dir: ${map_dir}"
 
-# rRNA
-featureCounts -t rRNA -a ../../genome/Nvec_v5_merged_annotation_sort.gtf  -o feat_counts_rRNA.txt Aligned.sortedByCoord.out.bam
+    # For each sample directory under the mapping directory
+    for sample_dir in $map_dir/*; do
+      echo "sample dir: ${sample_dir} "
+      
+      # Create seperate BAM with filtered results - filter for multimapped reads only present in primary alignment
+      samtools view -F 256 ${sample_dir}/Aligned.sortedByCoord.out.bam | grep -E "^\@|NH:i:[2-9]" > ${sample_dir}/multimapped_primealign.bam
+      # Create seperate BAM with filtered results - filter for multimapped reads present in both secondary and primary alignments
+      samtools view ${sample_dir}/Aligned.sortedByCoord.out.bam | grep -E "^\@|NH:i:[2-9]" > ${sample_dir}/multimapped_allalign.bam
 
-#mtDNA
-grep 'mtDNA' ../../genome/Nvec_v5_merged_annotation_sort.gtf > mtDNA_only.gtf
-featureCounts -t exon -a mtDNA_only.gtf -o feat_counts_mtDNA.txt Aligned.sortedByCoord.out.bam
-
+      # rRNA
+      featureCounts -t rRNA -a ${gtf_file} -o ${sample_dir}/feat_counts_rRNA.txt ${sample_dir}/Aligned.sortedByCoord.out.bam
+      featureCounts -t rRNA -a ${gtf_file} -o ${sample_dir}/feat_counts_rRNA_mmpa.txt ${sample_dir}/multimapped_primealign.bam
+      featureCounts -t rRNA -a ${gtf_file} -o ${sample_dir}/feat_counts_rRNA_mmaa.txt ${sample_dir}/multimapped_allalign.bam
+      
+      #mtDNA
+      grep 'mtDNA' ${gtf_file} > ${sample_dir}/mtDNA_only.gtf
+      featureCounts -t exon -a ${sample_dir}/mtDNA_only.gtf -o ${sample_dir}/feat_counts_mtDNA.txt ${sample_dir}/Aligned.sortedByCoord.out.bam
+      featureCounts -t exon -a ${sample_dir}/mtDNA_only.gtf -o ${sample_dir}/feat_counts_mtDNA_mmpa.txt ${sample_dir}/multimapped_primealign.bam
+      featureCounts -t exon -a ${sample_dir}/mtDNA_only.gtf -o ${sample_dir}/feat_counts_mtDNA_mmaa.txt ${sample_dir}/multimapped_allalign.bam
+      
+    done
+done
 
 
 ###############
